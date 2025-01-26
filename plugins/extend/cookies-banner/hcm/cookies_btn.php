45372d2d6a97c939a865688ac0bd0a16eed2ccef
pitva90 <venca.pitak@gmail.com>
2025-01-26 14:56:04 +0100

    remove btn click duplication

--- plugins/extend/cookies-banner/hcm/cookies_btn.php
+++ plugins/extend/cookies-banner/hcm/cookies_btn.php
@@ -6,15 +6,5 @@ return function() {
   $output .= '<span id="cookies-settings-in-text" class="cookies-settings-btn">'._lang('cookies.settings.intext').'</span>';
   $output .= '</div>';
 
-  $output .= '<script>';
-  $output .= 'document.addEventListener("DOMContentLoaded", () => {';
-  $output .= 'const consentSettingsBtnInText = document.getElementById("cookies-settings-in-text");';
-  $output .= 'consentSettingsBtnInText.addEventListener("click", function() {
-    consentBanner.classList.add("show-settings");
-    consentBanner.classList.remove("hidden");
-  }, false);';
-  $output .= '});';
-  $output .= '</script>';
-
   return $output;
 };
